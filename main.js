4855b465e45f7d42fc9516ccdc39854b1936f2bd
Marcos Mauricio de Melo Filho <marcos.filho@parvi.local>
2023-04-20 13:58:03 -0300

    add Back to Top

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
+
+var btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
 });
